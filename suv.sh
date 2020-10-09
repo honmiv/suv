ce44898b420416ba6ef5b600527c22fa414de5c0
honmiv <32744160+honmiv@users.noreply.github.com>
2020-10-09 11:25:32 +0300

    Create suv.sh
    add script

---
+++ suv.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+function createUser() {
+	
+	echo
+	read -p "What user you want to create? Enter its name: " username
+	
+	if [ -z $username ]; then
+		echo -e -n "\nUsername must not be empty"
+		return
+	fi
+	
+	echo 
+	read -p "With its home directory? [Y/n]: " createWithHomeDirectory
+	withDir=""
+	homeDir=""
+	if [ -z $createWithHomeDirectory ] || [ $createWithHomeDirectory != n ]; then
+		withDir="-m"
+		echo 
+		read -p "Enter home directory path: " homeDir
+		if [ -z $homeDir ]; then
+			echo -e -n "\nHome directory must not be empty"
+			return
+		fi
+		homeDir="-d ${homeDir}"
+	fi
+	
+	useradd -s $SHELL $withDir $homeDir $username
+	
+	echo
+	read -p "Set password for ${username}? [Y/n]: " setPassword
+	if [ $setPassword != n ]; then
+		echo
+		passwd $username
+		echo
+	fi
+}
+
+function delUser() {
+	echo -e -n "\nWhich user you want to delete? Enter its name: "
+	read username
+	echo -e -n "\nWith its home directory? [Y/n]: "
+	read delWithHomeDirectory
+	withDir="-r"
+	if [ $delWithHomeDirectory = n ] 
+	then
+		withDir=""
+	fi
+	echo
+	userdel $withDir $username
+	echo
+}
+
+if [ `whoami` != 'root' ]; then
+    echo "You must be root to use this manager."
+    exit
+fi
+
+echo "Welcome to SimpleUserManager [SUV]"
+
+while :
+do
+	echo -e -n "
+		\rYou can:
+		\r
+		\r1.create user
+		\r2.delete user
+		\r3.exit
+		\r
+		\rPlease, choose option [1/2/3]: "
+
+	read mainOption
+	
+	if [ $mainOption = 1 ]; then
+		createUser
+	elif [ $mainOption = 2 ]; then
+		delUser
+	elif [ $mainOption = 3 ]; then
+		break
+	fi
+done
